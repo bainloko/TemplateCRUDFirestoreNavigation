1f9fd3919a7d571d3cca3475416aec85f003cebd
Diego <diegonf25@gmail.com>
2022-03-25 21:03:47 -0300

    lista com refresh funcionando

--- firebase.js
+++ firebase.js
@@ -18,7 +18,7 @@ import "firebase/compat/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   
-  apiKey: "AIzaSyC5PpsbnndLOEgEuJv0tnPCCBrAHycaX6k",
+  coloque aqui suas configurações
 
   authDomain: "exemplo-video-11aa2.firebaseapp.com",
 
